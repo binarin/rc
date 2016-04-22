1ace6bcf1e8123eb29affb3ae239ad63d621bc22
Alexey Lebedeff <alebedev@mirantis.com>
2016-04-22 15:05:27 +0300

    Add erlang 18.3.1 for nix

--- nix/custom-packages.nix
+++ nix/custom-packages.nix
@@ -3,4 +3,11 @@ let
   pkgs = import <nixpkgs> { inherit system; };
 in rec {
   yandex-disk = pkgs.callPackage ./yandex-disk {};
+  erlang18 = pkgs.callPackage ./erlang/R18.nix {
+    Carbon = null;
+    Cocoa = null;
+    wxSupport = true;
+    javacSupport = true;
+    enableHipe = true;
+  };
 }
