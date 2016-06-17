d81dbb4a74d4f067b4bd99665147656020add455
Alexey Lebedeff <alebedev@mirantis.com>
2016-06-17 13:55:45 +0300

    Some custom.el and refactorings

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(custom-safe-themes
    (quote
-    ("f3d6a49e3f4491373028eda655231ec371d79d6d2a628f08d5aa38739340540b" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" default)))
+    ("40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" "a802c77b818597cc90e10d56e5b66945c57776f036482a033866f5f506257bca" "f3d6a49e3f4491373028eda655231ec371d79d6d2a628f08d5aa38739340540b" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" default)))
  '(edts-inhibit-package-check t)
  '(edts-man-root "/home/binarin/.emacs.d/edts/doc/18.1")
  '(eval-expression-print-level 4)
@@ -41,7 +41,7 @@
     ("~/org/mirantis.org" "~/org/mira-cal.org" "/home/binarin/org/personal.org" "/home/binarin/org/refile.org" "/home/binarin/org/subscriptions.org" "/home/binarin/.rc/emacs-config.org" "/home/binarin/org/ference.org")))
  '(package-selected-packages
    (quote
-    (nix-mode markdown-mode jammer zeal-at-point yaml-mode paredit evil-nerd-commenter elisp-slime-nav puppet-mode multiple-cursors expand-region helm-projectile persp-projectile god-mode edit-server smart-tab zenburn-theme flx-ido yasnippet web-mode vimish-fold magit js2-mode shm hindent haskell-mode auto-highlight-symbol eproject auto-complete erlang electric-operator corral projectile perspective helm pt bookmark+ mu4e-maildirs-extension htmlize ws-butler ace-jump-mode key-chord undo-tree hydra zoom-frm visual-fill-column smart-mode-line-powerline-theme rich-minority highlight-parentheses request req-package f anaphora)))
+    (cider nix-mode markdown-mode jammer zeal-at-point yaml-mode paredit evil-nerd-commenter elisp-slime-nav puppet-mode multiple-cursors expand-region helm-projectile persp-projectile god-mode edit-server smart-tab zenburn-theme flx-ido yasnippet web-mode vimish-fold magit js2-mode shm hindent haskell-mode auto-highlight-symbol eproject auto-complete erlang electric-operator corral projectile perspective helm pt bookmark+ mu4e-maildirs-extension htmlize ws-butler ace-jump-mode key-chord undo-tree hydra zoom-frm visual-fill-column smart-mode-line-powerline-theme rich-minority highlight-parentheses request req-package f anaphora)))
  '(safe-local-variable-values
    (quote
     ((nxml-child-indent . 2)
