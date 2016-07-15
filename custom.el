bf1274d53a1295e8d6e7db56b5ae35397668b737
Alexey Lebedeff <alebedev@mirantis.com>
2016-07-15 13:55:31 +0300

    Change default font to iosevka

--- custom.el
+++ custom.el
@@ -6,11 +6,13 @@
  '(custom-safe-themes
    (quote
     ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
- '(edts-inhibit-package-check t))
+ '(edts-inhibit-package-check t)
+ '(safe-local-variable-values (quote ((allout-layout . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F"))))
  '(mode-line ((t (:background "#2B2B2B" :foreground "#8FB28F" :box (:line-width -1 :style released-button) :height 0.7))))
  '(mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width -1 :style released-button) :height 0.7)))))
