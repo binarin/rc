5dc9512d913299ad4c02afe4007782e9a7d1c7cd
Alexey Lebedeff <alebedev@mirantis.com>
2016-07-29 13:44:45 +0300

    Remove customizations of default face

--- custom.el
+++ custom.el
@@ -13,6 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F"))))
- '(mode-line ((t (:background "#2B2B2B" :foreground "#8FB28F" :box (:line-width -1 :style released-button) :height 0.7))))
- '(mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width -1 :style released-button) :height 0.7)))))
+ '(mode-line ((t (:height 0.7))))
+ '(mode-line-inactive ((t (:height 0.7)))))
