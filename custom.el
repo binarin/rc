8c5dd8adb650ca66d44a9b9704b2ec704ed4c175
Alexey Lebedeff <alebedev@mirantis.com>
2017-05-05 22:28:36 +0300

    Change flyspell incorrect face to a less intrusive color

--- custom.el
+++ custom.el
@@ -13,5 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flyspell-incorrect ((t (:inherit nil :underline (:color "orange" :style wave)))))
  '(mode-line ((t (:height 0.7))))
  '(mode-line-inactive ((t (:height 0.7)))))
