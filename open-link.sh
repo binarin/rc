5b619e1b55ee8afd3c1357e13e6f8b61f7302101
Alexey Lebedeff <alebedev@mirantis.com>
2017-10-11 18:12:52 +0200

    More things to open in chromium

--- open-link.sh
+++ open-link.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env zsh
 local url="$1"
 
-if [[ $url =~ 'booking\.com' ]]; then
+if [[ $url =~ 'booking\.com'
+       || $url =~ 'booking\.facebook\.com'
+       || $url =~ '#label/Lists%2Fsp\.data\.cron'
+   ]]; then
     exec chromium "$@"
 else
     exec firefox "$@"
