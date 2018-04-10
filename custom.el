9003a118871acd2f0d0dd2f7d7a8f91dcd714060
Alexey Lebedeff <binarin@binarin.ru>
2018-04-10 14:24:37 +0200

    Fix mode-line size

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(edts-inhibit-package-check t)
  '(safe-local-variable-values (quote ((allout-layout . t)))))
 (custom-set-faces
@@ -14,6 +14,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flyspell-incorrect ((t (:inherit nil :underline (:color "orange" :style wave)))))
- '(mode-line ((t :foreground "gray60" :background "black" :box (:line-width -1 :color "Black"))))
- '(mode-line-inactive ((((background dark)) :foreground "gray60" :background "Black" :slant italic :box (:line-width -3 :color "black")) (((background light)) :foreground "gray60" :background "Black" :slant italic :box (:line-width -2 :color "white"))))
+ '(mode-line ((t (:background "black" :foreground "gray60" :box (:line-width -1 :color "Black") :height 0.6))))
+ '(mode-line-inactive ((t (:background "Black" :foreground "gray60" :box (:line-width -3 :color "black") :slant italic :height 0.6))))
  '(org-agenda-current-time ((t (:inherit org-time-grid :foreground "green")))))
