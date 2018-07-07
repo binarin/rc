fbd454f0a766b792d09c36b23bc5d38cee252ea2
Alexey Lebedeff <binarin@binarin.ru>
2018-07-07 08:17:50 +0200

    Upgrade taffybar

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -3,12 +3,13 @@ import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 import System.Taffybar
-import System.Taffybar.Systray
-import System.Taffybar.TaffyPager
-import System.Taffybar.Battery
-import System.Taffybar.SimpleClock
-import System.Taffybar.Widgets.PollingGraph
-import System.Information.CPU
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget.Battery
+import System.Taffybar.Widget.SimpleClock
+import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Information.CPU
+
+import System.Taffybar.Widget.SNITray
 
 cpuCallback = do
   (_, systemLoad, totalLoad) <- cpuLoad
@@ -21,11 +22,16 @@ main = do
                                   , graphLabel = Just "cpu"
                                   }
       clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
-      pager = taffyPagerNew defaultPagerConfig
-      tray = systrayNew
+      -- pager = taffyPagerNew defaultPagerConfig
+      tray = sniTrayNew
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
-      battery = batteryBarNew defaultBatteryConfig 30
-  defaultTaffybar defaultTaffybarConfig { startWidgets = [ pager ]
-                                        , endWidgets = [ tray, clock, cpu, battery ]
-                                        , monitorNumber = chosenMonitorNumber
-                                        }
+      -- battery = batteryBarNew defaultBatteryConfig 30
+      simpleConfig = defaultSimpleTaffyConfig
+        { startWidgets = [ -- pager
+                         ]
+        , endWidgets = [ tray, clock, cpu
+                       -- , battery
+                       ]
+        , monitorsAction = pure [chosenMonitorNumber]
+        }
+  simpleTaffybar simpleConfig
