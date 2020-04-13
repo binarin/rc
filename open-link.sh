5c469bc6ad013020406e0ca274b44e391344cf69
Alexey Lebedeff <binarin@binarin.ru>
2020-04-13 15:52:51 +0200

    Open workspace links in chrome

--- open-link.sh
+++ open-link.sh
@@ -4,6 +4,7 @@ local url="$1"
 if [[ $url =~ 'booking\.com'
        || $url =~ 'booking\.facebook\.com'
        || $url =~ '#label/Lists%2Fsp\.data\.cron'
+       || $url =~ 'booking\.workplace\.com'
    ]]; then
     exec google-chrome-stable "$@"
 else
