f3c1e8f3a5d30b783406b9e9bd1079a8846c1a30
Alexey Lebedeff <binarin@binarin.ru>
2020-04-24 10:08:28 +0200

    WIP Fri, 24 Apr 2020 10:08:28 +0200

---
+++ xmonad-config/gi-gdkx.nix
@@ -0,0 +1,22 @@
+{ mkDerivation, base, bytestring, Cabal, containers, gi-cairo
+, gi-gdk, gi-gio, gi-gobject, gi-xlib, gtk3, haskell-gi
+, haskell-gi-base, haskell-gi-overloading, stdenv, text
+, transformers
+}:
+mkDerivation {
+  pname = "gi-gdkx11";
+  version = "3.0.8";
+  sha256 = "02885b4786b9a72e0b2621654e74f3f48e6c2bdf84c7e52a512654003435e26f";
+  setupHaskellDepends = [
+    base Cabal gi-cairo gi-gdk gi-gio gi-gobject gi-xlib haskell-gi
+  ];
+  libraryHaskellDepends = [
+    base bytestring containers gi-cairo gi-gdk gi-gio gi-gobject
+    gi-xlib haskell-gi haskell-gi-base haskell-gi-overloading text
+    transformers
+  ];
+  libraryPkgconfigDepends = [ gtk3 ];
+  homepage = "https://github.com/haskell-gi/haskell-gi";
+  description = "GdkX11 bindings";
+  license = stdenv.lib.licenses.lgpl21;
+}
